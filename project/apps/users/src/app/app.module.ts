172ab9b87ab86b18d18b8ed251246bf8f8afaccb
Evgeny Bochkarev <evgeniy.bochkarev@netwrix.com>
2023-04-04 21:55:22 +0400

    Implement docker image for mongo db

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [UserModule, AuthenticationModule],
+  imports: [
+    UserModule,
+    AuthenticationModule,
+    MongooseModule.forRoot(
+      'mongodb://admin:pass@localhost:27017/users?authSource=admin',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
